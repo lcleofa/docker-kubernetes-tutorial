041141cf0c649cd8f49a714acaee90e32ed0db6e
lcleofa <lcleofa@yahoo.com>
2019-05-18 09:49:09 +0200

    updated h1 text to hola

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use('*', (req, res, next) => {
-    res.send('<h1>Hello from express app powered by docker and kubernetes for Stedin-net datalake DLKP</h1>')
+    res.send('<h1>Hola from express app powered by docker and kubernetes for Stedin-net datalake DLKP</h1>')
 })
 
 app.listen(4000, () => {
