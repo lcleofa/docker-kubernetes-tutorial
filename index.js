93a40a2c7458bc7264d2ed94dd3ed799d4b0df3c
lcleofa <lcleofa@yahoo.com>
2019-05-25 13:10:53 +0200

    hola cdo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use('*', (req, res, next) => {
-    res.send('<h1>Hi Afrah and Diederik, from express app powered by docker and kubernetes for Stedin-net datalake DLKP</h1>')
+    res.send('<h1>Hi cdo Afrah and Diederik, from express app powered by docker and kubernetes for Stedin-net datalake DLKP</h1>')
 })
 
 app.listen(4000, () => {
